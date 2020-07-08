3c4592b0f3337a88c7e06658c4db3d285d6b374a
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-08 11:37:10 -0600

    cities ui hooked up to api

--- client/src/Screens/Cities/Cities.js
+++ client/src/Screens/Cities/Cities.js
@@ -1,33 +1,58 @@
 import React from "react";
 import { Form, TableHeader, TableBody, Table } from "semantic-ui-react";
+import Axios from "axios";
 
+const cities = ["draper", "sandy", "SLC"];
+const options = cities.map((c) => {
+  return { key: c, text: c, value: c };
+});
+
+const p = [
+  {
+    price: 123234.0,
+    street: "123456 maywood",
+    beds: 12,
+    baths: 3,
+    sq_ft: 1232,
+  },
+  {
+    price: 1233234.0,
+    street: "123456 lynwood",
+    beds: 3,
+    baths: 5,
+    sq_ft: 1200,
+  },
+];
 export default class Cities extends React.Component {
-
   state = {
-    properties: [
-      {
-        price: 123234.0,
-        street: "123456 maywood",
-        beds: 12,
-        baths: 3,
-        sq_ft: 1232,
-      },
-      {
-        price: 1233234.0,
-        street: "123456 lynwood",
-        beds: 3,
-        baths: 5,
-        sq_ft: 1200,
-      },
-    ],
+    city: "",
+    properties: [],
   };
-  
+  handleChange = (e, { value }) => {
+    console.log(e);
+    console.log(value);
+    // maybe api call or set state
+    this.setState({ city: value, properties: [] }, () => {
+      this.getProperties();
+    });
+  };
+  getProperties() {
+    const { city } = this.state;
+    Axios.get(`/api/cities/${city}`)
+      .then((res) => {
+        console.log(res);
+        this.setState({ properties: res.data });
+      })
+      .catch((e) => {
+        //TODO handle error
+      });
+  }
   render() {
     const { properties } = this.state;
     return (
       <div>
         {/* select Input */}
-        <Form.Select />
+        <Form.Select options={options} onChange={this.handleChange} />
         {/* Table */}
         <Table>
           <TableHeader>
