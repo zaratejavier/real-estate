3e033ed0568ec2891e6baae33549ebaa01024cd3
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-08 10:59:50 -0600

    added cities component, route is working

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Container } from "semantic-ui-react";
 import Navbar from "./Components/NavBar";
 import { Switch, Route } from "react-router-dom";
 import Available from "./Screens/Available/Available";
+import Cities from "./Screens/Cities/Cities";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <Switch>
           <Route exact path="/" component={() => <h1>Home</h1>} />
           <Route exact path="/available" component={Available} />
+          <Route exact path="/cities" component={Cities} />
           <Route exact path="/city_cost" component={() => <h1>city_cost</h1>} />
           {/* 
           <Route exact path="/available" component={Available} />
-          <Route exact path="/cities" component={Cities} />
+          
           <Route exact path="/find_home" component={FindHome} /> */}
         </Switch>
       </Container>
---
+++ client/src/Screens/Cities/Cities.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Cities extends React.Component{
+  
+  render() {
+    return (
+      <div>
+        <h1>Cities here</h1>
+      </div>
+    )
+  }
+}
+
+export default Cities
