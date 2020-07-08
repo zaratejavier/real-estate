9b76dd4a49573c3c827723698f1a08b39a7ac447
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-08 11:12:12 -0600

    have dummy properties rendering on table

--- client/src/Screens/Cities/Cities.js
+++ client/src/Screens/Cities/Cities.js
@@ -1,14 +1,55 @@
-import React from "react"
+import React from "react";
+import { Form, TableHeader, TableBody, Table } from "semantic-ui-react";
 
-class Cities extends React.Component{
+export default class Cities extends React.Component {
+
+  state = {
+    properties: [
+      {
+        price: 123234.0,
+        street: "123456 maywood",
+        beds: 12,
+        baths: 3,
+        sq_ft: 1232,
+      },
+      {
+        price: 1233234.0,
+        street: "123456 lynwood",
+        beds: 3,
+        baths: 5,
+        sq_ft: 1200,
+      },
+    ],
+  };
   
   render() {
+    const { properties } = this.state;
     return (
       <div>
-        <h1>Cities here</h1>
+        {/* select Input */}
+        <Form.Select />
+        {/* Table */}
+        <Table>
+          <TableHeader>
+            <Table.HeaderCell>Price</Table.HeaderCell>
+            <Table.HeaderCell>Beds</Table.HeaderCell>
+            <Table.HeaderCell>Bath</Table.HeaderCell>
+            <Table.HeaderCell>Square Feet</Table.HeaderCell>
+            <Table.HeaderCell>Address</Table.HeaderCell>
+          </TableHeader>
+          <TableBody>
+            {properties.map((p) => (
+              <Table.Row>
+                <Table.Cell>{p.price}</Table.Cell>
+                <Table.Cell>{p.beds}</Table.Cell>
+                <Table.Cell>{p.baths}</Table.Cell>
+                <Table.Cell>{p.sq_ft}</Table.Cell>
+                <Table.Cell>{p.street}</Table.Cell>
+              </Table.Row>
+            ))}
+          </TableBody>
+        </Table>
       </div>
-    )
+    );
   }
 }
-
-export default Cities
