f817dae10f834e7ca2bb1f335235d9ca73f9da82
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-09 10:46:08 -0600

    findhome component working without backend right now

--- client/src/Screens/FindHome/FindHome.js
+++ client/src/Screens/FindHome/FindHome.js
@@ -1,11 +1,74 @@
-import React,{useState, useEffect} from "react"
-import { Form } from "semantic-ui-react"
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { Form } from "semantic-ui-react";
+import axios from "axios";
 
 export default function (props) {
+  const [agents, setAgents] = useState([]);
+  const [buyers, setBuyers] = useState([]);
+  const [showBuyers, setShowBuyers] = useState(false);
+
+  async function getAgents() {
+    //TODO: axios call here
+    try {
+      //assuming when api is ready
+      let res = await axios.get("/api/agentsxx");
+      setAgents(res.data);
+      setShowBuyers(true);
+    } catch (e) {
+      // normally you don't want to do this here, but I am expecting this call to fail
+      setAgents([
+        { first_name: "agent", last_name: "1", id: 1 },
+        { first_name: "agent", last_name: "2", id: 2 },
+      ]);
+      setShowBuyers(true);
+    }
+  }
+  
+  useEffect(() => {
+    getAgents();
+  }, []);
+  function getAgentsOptions() {
+    return agents.map((agent) => ({
+      key: agent.id,
+      text: `${agent.first_name} ${agent.last_name}`,
+      value: agent.id,
+    }));
+  }
+
+  function getBuyersOptions() {
+    return buyers.map((buyer) => ({
+      key: buyer.id,
+      text: `${buyer.first_name} ${buyer.last_name}`,
+      value: buyer.id,
+    }));
+  }
+
+  async function getBuyers(e, { value }) {
+    console.log(value);
+    const agentId = value;
+    //TODO axios call using value(ie agent id) to get buyers
+    try {
+      const res = await axios.get(`/api/xagent_buyersx/${agentId}`);
+      setBuyers(res.data);
+    } catch (e) {
+      setBuyers([
+        { first_name: "buyer", last_name: "1", id: 1 },
+        { first_name: "buyer", last_name: "2", id: 2 },
+      ]);
+    }
+  }
+
+  function getBuyersList(e, { value }) {
+    console.log(value);
+  }
+
   return (
     <div>
       <h1>Find Home</h1>
+      <Form.Select options={getAgentsOptions()} onChange={getBuyers} />
+      {showBuyers && (
+        <Form.Select options={getBuyersOptions()} onChange={getBuyersList} />
+      )}
     </div>
-  )
+  );
 }
